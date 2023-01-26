2ed832eccea189de1efd831c2f9dda2cb07ab1be
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-01-26 17:57:32 +0200

    BoardsList component created

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,13 @@
-import { Board } from "App/components/Board/Board";
+// import { Board } from "App/components/Board/Board";
 import { DragAndDropProvider } from "drag-and-drop/components/DragAndDropProvider";
+import { BoardsList } from "./components/BoardsList/BoardsList";
 import { AppStateProvider } from "./state/components/AppStateProvider";
 
 export const App = (): JSX.Element => (
   <DragAndDropProvider>
     <AppStateProvider>
-      <Board title="Test Board" />
+      <BoardsList></BoardsList>
+      {/* <Board title="Test Board" /> */}
     </AppStateProvider>
   </DragAndDropProvider>
 );
---
+++ src/App/components/BoardsList/BoardsList.tsx
@@ -0,0 +1,3 @@
+export const BoardsList = () => {
+  return <h3>BoardsList</h3>;
+};

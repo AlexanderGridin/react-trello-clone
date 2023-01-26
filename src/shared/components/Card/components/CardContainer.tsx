ec1fcd1805e780aa3f2865bb16226f74035685cc
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-01-26 17:57:11 +0200

    min-height was added to CardContainer

--- src/shared/components/Card/components/CardContainer.tsx
+++ src/shared/components/Card/components/CardContainer.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 interface CardContainerProps {
+  minHeight?: number;
   backgroundColor?: string;
 }
 
@@ -9,5 +10,6 @@ export const CardContainer = styled.div<CardContainerProps>`
   padding: 7px 12px;
   border-radius: 3px;
   background-color: ${({ backgroundColor }) => backgroundColor || "#FFF"};
+	min-height: ${({ minHeight }) => minHeight || "auto"}
   box-shadow: #091e4240 0px 1px 0px 0px;
 `;

375feafcb7dbbd9f6f212e0a5ec88525c8a370f6
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-01-26 15:18:48 +0200

    moveTaskReducer refactored

--- src/App/components/Task/state/reducers/moveTaskReducer.ts
+++ src/App/components/Task/state/reducers/moveTaskReducer.ts
@@ -1,10 +1,8 @@
-import { TaskModel } from "App/components/Task/models/TaskModel";
 import { TasksListModel } from "App/components/TasksList/models/TasksListModel";
 import { AppState } from "App/state/models/AppState";
-import { moveItemAfterArrayItem } from "shared/utils/array/moveItemAfterArrayItem";
-import { moveItemBeforeArrayItem } from "shared/utils/array/moveItemBeforeArrayItem";
-import { removeItemFromArray } from "shared/utils/array/removeItemFromArray";
 import { MoveTaskAction } from "../actions/moveTask";
+import { removeTaskFromList } from "./utils/removeTaskFromList";
+import { replaceTaskInList } from "./utils/replaceTaskInList";
 
 export const moveTaskReducer = (
   state: AppState,
@@ -14,57 +12,18 @@ export const moveTaskReducer = (
 
   const tasksLists = state.tasksLists.map((list: TasksListModel) => {
     if (list.id === taskToReplace.listId) {
-      const taskToMoveIndex = list.tasks.findIndex(
-        ({ id }: TaskModel) => id === taskToMove.id
-      );
-      const taskToReplaceIndex = list.tasks.findIndex(
-        ({ id }: TaskModel) => id === taskToReplace.id
-      );
-
-      if (taskToMoveIndex < 0 || taskToMoveIndex > taskToReplaceIndex) {
-        const tasks: TaskModel[] = moveItemBeforeArrayItem<TaskModel>({
-          array: list.tasks,
-          item: {
-            ...taskToMove,
-            listId: list.id,
-          },
-          arrayItem: taskToReplace,
-          uniqueKey: "id",
-        });
-
-        return {
-          ...list,
-          tasks,
-        };
-      }
-
-      if (taskToMoveIndex < taskToReplaceIndex) {
-        const tasks: TaskModel[] = moveItemAfterArrayItem<TaskModel>({
-          array: list.tasks,
-          item: {
-            ...taskToMove,
-            listId: list.id,
-          },
-          arrayItem: taskToReplace,
-          uniqueKey: "id",
-        });
-
-        return {
-          ...list,
-          tasks,
-        };
-      }
+      return replaceTaskInList({
+        list,
+        task: taskToMove,
+        taskToReplace,
+      });
     }
 
     if (list.id === taskToMove.listId) {
-      return {
-        ...list,
-        tasks: removeItemFromArray<TaskModel>({
-          array: list.tasks,
-          item: taskToMove,
-          uniqueKey: "id",
-        }),
-      };
+      return removeTaskFromList({
+        list,
+        task: taskToMove,
+      });
     }
 
     return list;

d185a67b3662c39993f450921785a163d5b4740f
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-01-29 22:19:51 +0200

    make BoardsPage visible

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,13 +1,13 @@
 import { DragAndDropProvider } from "drag-and-drop/components/DragAndDropProvider";
 import { BoardPage } from "./pages/BoardPage/BoardPage";
-// import { BoardsPage } from "./pages/BoardsPage/BoardsPage";
+import { BoardsPage } from "./pages/BoardsPage/BoardsPage";
 import { AppStateProvider } from "./state/components/AppStateProvider";
 
 export const App = (): JSX.Element => (
   <DragAndDropProvider>
     <AppStateProvider>
-      <BoardPage title="Test Board" />
-      {/* <BoardsPage /> */}
+      {/* <BoardPage title="Test Board" /> */}
+      <BoardsPage />
     </AppStateProvider>
   </DragAndDropProvider>
 );

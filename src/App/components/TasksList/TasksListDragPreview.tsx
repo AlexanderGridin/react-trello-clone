16f3fce99cd4e825fe5f7fc47da906f46db97f3e
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-01-29 22:15:15 +0200

    TasksList updated

--- src/App/components/TasksList/TasksListDragPreview.tsx
+++ src/App/components/TasksList/TasksListDragPreview.tsx
@@ -9,22 +9,29 @@ import { Task } from "../Task/Task";
 export const TasksListDragPreview = ({ list }: TasksListProps) => {
   const { remove, addTask, removeTask } = useTasksListActions(list);
 
+  const header = <TasksListHeader onRemove={remove} list={list} />;
+
+  const content = list.tasks.length ? (
+    <ul className="plain-list">
+      {list.tasks.map((task: TaskModel) => (
+        <li key={task.id} className="mb">
+          <Card>
+            <Task task={task} onRemove={removeTask(task)}></Task>
+          </Card>
+        </li>
+      ))}
+    </ul>
+  ) : null;
+
+  const footer = <AddTask onAdd={addTask}>+ Add new task</AddTask>;
+
   return (
     <Card
-      header={<TasksListHeader onRemove={remove} list={list} />}
-      footer={<AddTask onAdd={addTask}>+ Add new task</AddTask>}
+      slotHeader={header}
+      slotContent={content}
+      slotFooter={footer}
       backgroundColor="#ebecf0"
       className="drag-preview"
-    >
-      <ul className="plain-list">
-        {list.tasks.map((task: TaskModel) => (
-          <li key={task.id} className="mb">
-            <Card>
-              <Task task={task} onRemove={removeTask(task)}></Task>
-            </Card>
-          </li>
-        ))}
-      </ul>
-    </Card>
+    />
   );
 };

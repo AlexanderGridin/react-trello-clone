61ffead4fe625c76429d4fb83b30798a1418ace3
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-01-30 17:16:11 +0200

    minor updates

--- src/App/pages/BoardPage/BoardPage.tsx
+++ src/App/pages/BoardPage/BoardPage.tsx
@@ -4,24 +4,27 @@ import { useBoardPageActions } from "./hooks/useBoardPageActions";
 import { AppPageLayout } from "App/components/AppPageLayout/AppPageLayout";
 import { BoardPageDragLayer } from "./components/BoardPageDragLayer";
 import { BoardPageHeader } from "./components/BoardPageHeader";
-import { BoardModel } from "App/components/Board/models/BoardModel";
 import { BoardPageCell } from "./components/BoardPageCell";
 import { BoardPageContent } from "./components/BoardPageContent";
 import { BoardPageTasksLists } from "./components/BoardPageTasksLists";
+import { BoardWithTasksListsModel } from "App/components/Board/models/BoardWithTasksListsModel";
 
 export const BoardPage = () => {
   const { tasksLists } = useAppState();
   const { addTasksList } = useBoardPageActions();
 
   // TODO: refactor to use board from the API
-  const board = { title: "Test board title" } as BoardModel;
+  const board = {
+    title: "Test board title",
+    tasksLists,
+  } as BoardWithTasksListsModel;
 
   return (
     <AppPageLayout slotHeader={<BoardPageHeader board={board} />}>
       <BoardPageContent>
         <BoardPageDragLayer />
 
-        <BoardPageTasksLists lists={tasksLists} />
+        <BoardPageTasksLists lists={board.tasksLists} />
 
         <BoardPageCell className="mr-0">
           <AddList onAdd={addTasksList}>+ Add new list</AddList>

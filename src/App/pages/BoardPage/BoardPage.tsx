8ed85fdb6374778fae2c16e7c5e7fd75fea0528b
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-01-30 17:12:42 +0200

    BoardPage updates

--- src/App/pages/BoardPage/BoardPage.tsx
+++ src/App/pages/BoardPage/BoardPage.tsx
@@ -2,35 +2,26 @@ import { useAppState } from "App/state/hooks/useAppState";
 import { AddList } from "./components/AddList/AddList";
 import { useBoardPageActions } from "./hooks/useBoardPageActions";
 import { AppPageLayout } from "App/components/AppPageLayout/AppPageLayout";
-import { TasksListModel } from "App/components/TasksList/models/TasksListModel";
-import { TasksList } from "App/components/TasksList/TasksList";
 import { BoardPageDragLayer } from "./components/BoardPageDragLayer";
 import { BoardPageHeader } from "./components/BoardPageHeader";
 import { BoardModel } from "App/components/Board/models/BoardModel";
 import { BoardPageCell } from "./components/BoardPageCell";
 import { BoardPageContent } from "./components/BoardPageContent";
+import { BoardPageTasksLists } from "./components/BoardPageTasksLists";
 
-interface BoardPageProps {
-  title?: string;
-}
-
-export const BoardPage = ({ title = "Test board title" }: BoardPageProps) => {
+export const BoardPage = () => {
   const { tasksLists } = useAppState();
   const { addTasksList } = useBoardPageActions();
 
   // TODO: refactor to use board from the API
-  const board = { title } as BoardModel;
+  const board = { title: "Test board title" } as BoardModel;
 
   return (
     <AppPageLayout slotHeader={<BoardPageHeader board={board} />}>
       <BoardPageContent>
         <BoardPageDragLayer />
 
-        {tasksLists.map((list: TasksListModel) => (
-          <BoardPageCell key={list.id}>
-            <TasksList list={list} />
-          </BoardPageCell>
-        ))}
+        <BoardPageTasksLists lists={tasksLists} />
 
         <BoardPageCell className="mr-0">
           <AddList onAdd={addTasksList}>+ Add new list</AddList>

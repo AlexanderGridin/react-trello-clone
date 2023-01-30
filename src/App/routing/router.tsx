469b1395c38a804d16219d0ca69ec7571d0b9e22
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-01-31 01:28:33 +0200

    router configured

---
+++ src/App/routing/router.tsx
@@ -0,0 +1,18 @@
+import { AppLayout } from "App/components/AppLayout";
+import { BoardPage } from "App/pages/BoardPage/BoardPage";
+import { BoardsPage } from "App/pages/BoardsPage/BoardsPage";
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      { index: true, element: <BoardsPage /> },
+      {
+        path: "boards/:id",
+        element: <BoardPage />,
+      },
+    ],
+  },
+]);

b8ff324b63b50ace6a376c2eeb334f08c97ba569
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-02-01 12:53:08 +0200

    BoardPage updated

--- src/App/pages/BoardPage/hooks/useBoardPageFeatures.ts
+++ src/App/pages/BoardPage/hooks/useBoardPageFeatures.ts
@@ -1,7 +1,7 @@
 import { BoardModel } from "App/entities/Board/BoardModel";
 import { useTasksListDispatchers } from "App/widgets/TasksList/state/hooks/useTasksListDispatchers";
 
-export const useBoardPageActions = (board: BoardModel) => {
+export const useBoardPageFeatures = (board: BoardModel) => {
   const { dispatchAddTasksList } = useTasksListDispatchers();
   const addTasksList = (title: string) => dispatchAddTasksList(title, board.id);
 

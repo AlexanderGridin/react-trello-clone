03d0c8efb0e4e83129de389f102c13fd413adfac
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-02-01 21:05:48 +0200

    Icon updated

--- src/shared/components/Icon/components/IconContainer.tsx
+++ src/shared/components/Icon/components/IconContainer.tsx
@@ -1,8 +1,6 @@
 import styled from "styled-components";
 
-export const IconContainer = styled.span<{ dark?: boolean }>`
-  color: ${({ dark }) => (dark ? "black" : "white")};
-  font-size: 1rem;
+export const IconContainer = styled.span`
   line-height: 1;
   display: block;
 `;

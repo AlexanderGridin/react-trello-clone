80a43f8bbc354d4a85947f5b17d88d0e2e33d299
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-02-03 13:27:25 +0200

    AddButton renamed to AddButton container

--- src/App/components/AddButtonContainer/AddButtonContainer.tsx
+++ src/App/components/AddButtonContainer/AddButtonContainer.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { ButtonContainer } from "shared/components/Button/components/ButtonContainer";
 
-export const AddButton = styled(ButtonContainer)`
+export const AddButtonContainer = styled(ButtonContainer)`
   text-align: left;
   padding: 12px 10px;
   background-color: #ffffff3d;

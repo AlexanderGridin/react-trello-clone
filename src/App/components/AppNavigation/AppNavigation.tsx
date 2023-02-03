fe3d0d72dcc47aece106f7ec90cabab9806826e3
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-02-03 19:12:15 +0200

    AppNavigation updated

--- src/App/components/AppNavigation/AppNavigation.tsx
+++ src/App/components/AppNavigation/AppNavigation.tsx
@@ -4,20 +4,27 @@ import { Icon } from "shared/components/Icon/Icon";
 import { MaterialIcon } from "shared/enums/MaterialIcon";
 import style from "./AppNavigation.module.css";
 
+const setClassName = ({ isActive }: { isActive: boolean }) =>
+  isActive ? `${style.LinkActive} ${style.Link}` : style.Link;
+
 export const AppNavigation = () => {
   return (
     <nav>
       <ul className={`plain-list ${style.List}`}>
         <li className={style.ListItem}>
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              isActive ? `${style.LinkActive} ${style.Link}` : style.Link
-            }
-          >
+          <NavLink to="/" className={setClassName}>
             <FlexContainer>
               <Icon className="mr" icon={MaterialIcon.Home} />
-              <span> Home</span>
+              <span> Boards</span>
+            </FlexContainer>
+          </NavLink>
+        </li>
+
+        <li className={style.ListItem}>
+          <NavLink to="/boards/favorite" className={setClassName}>
+            <FlexContainer>
+              <Icon className="mr" icon={MaterialIcon.Star} />
+              <span> Favorite</span>
             </FlexContainer>
           </NavLink>
         </li>

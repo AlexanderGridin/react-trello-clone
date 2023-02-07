10e29df4a0392228283f839a4ab4e179936e6fd3
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-02-07 21:13:06 +0200

    fix dissapearing of the AddTask

--- src/App/widgets/ListOfTasks/ListOfTasks.tsx
+++ src/App/widgets/ListOfTasks/ListOfTasks.tsx
@@ -18,19 +18,17 @@ export const ListOfTasks = ({
 }: ListOfTasksProps) => {
   const { addTask } = useListOfTasksFeatures(boardId, listId);
 
-  if (!tasks.length) {
-    return null;
-  }
-
   return (
     <>
-      <ul className="plain-list">
-        {tasks.map((task: TaskViewModel) => (
-          <li key={task.id} className="mb">
-            <ListOfTasksListItem task={task} key={task.id} />
-          </li>
-        ))}
-      </ul>
+      {tasks.length > 0 && (
+        <ul className="plain-list">
+          {tasks.map((task: TaskViewModel) => (
+            <li key={task.id} className="mb">
+              <ListOfTasksListItem task={task} key={task.id} />
+            </li>
+          ))}
+        </ul>
+      )}
 
       {isShowAddTask && <AddTask onAdd={addTask} />}
     </>

e1f9db1107dd253f82f6a8a07d76d527738e08ca
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-02-07 11:55:54 +0200

    Sidebar added

---
+++ src/App/widgets/Sidebar/components/SidebarLayout/SidebarLayout.tsx
@@ -0,0 +1,21 @@
+import { PropsWithChildren, ReactNode } from "react";
+import style from "./SidebardLayout.module.css";
+
+export interface SidebarLayoutProps extends PropsWithChildren {
+  slotHeader?: ReactNode;
+  slotFooter?: ReactNode;
+}
+
+export const SidebarLayout = ({
+  slotHeader,
+  children,
+  slotFooter,
+}: SidebarLayoutProps) => {
+  return (
+    <div className={style.container}>
+      {slotHeader && <div className={style.header}>{slotHeader}</div>}
+      {children && <div className={style.content}>{children}</div>}
+      {slotFooter && <div className={style.footer}>{slotFooter}</div>}
+    </div>
+  );
+};

73ce6008fffcb8a9e366228ec8aefd1c47aa2a2a
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-02-07 17:36:45 +0200

    TasksList updated

--- src/App/widgets/TasksList/TasksList.tsx
+++ src/App/widgets/TasksList/TasksList.tsx
@@ -2,35 +2,34 @@ import { DndCard } from "App/components/DndCard/DndCard";
 import { mapTasksListToDraggedItem } from "App/entities/TasksList/mappers/mapTasksListToDraggedItem";
 import { TasksListViewModel } from "App/entities/TasksList/TasksListViewModel";
 import { Card } from "shared/components/Card/Card";
-import { AddTask } from "../AddTask/AddTask";
+import { ListOfTasks } from "../ListOfTasks/ListOfTasks";
 import { TasksListHeader } from "./components/TasksListHeader/TasksListHeader";
-import { TasksListItems } from "./components/TasksListItems/TasksListItems";
 import { useTasksListFeatures } from "./hooks/useTasksListFeatures";
 
 export interface TasksListProps {
   list: TasksListViewModel;
-  isShowAddTask?: boolean;
   isDragPreview?: boolean;
 }
 
-export const TasksList = ({
-  list,
-  isShowAddTask = false,
-  isDragPreview = false,
-}: TasksListProps) => {
+export const TasksList = ({ list, isDragPreview = false }: TasksListProps) => {
   const BACKGROUD_COLOR = "#D8DEE9";
-  const { addTask, dropOnList } = useTasksListFeatures(list);
+  const { dropOnList } = useTasksListFeatures(list);
 
   const header = <TasksListHeader list={list} />;
-  const content = <TasksListItems tasks={list.tasks} />;
-  const footer = isShowAddTask ? <AddTask onAdd={addTask} /> : null;
+  const content = (
+    <ListOfTasks
+      boardId={list.boardId}
+      listId={list.id}
+      tasks={list.tasks}
+      isShowAddTask
+    />
+  );
 
   if (isDragPreview) {
     return (
       <Card
         slotHeader={header}
         slotContent={content}
-        slotFooter={footer}
         backgroundColor={BACKGROUD_COLOR}
         className="drag-preview"
       />
@@ -41,7 +40,6 @@ export const TasksList = ({
     <DndCard
       slotHeader={header}
       slotContent={content}
-      slotFooter={footer}
       backgroundColor={BACKGROUD_COLOR}
       draggedItem={mapTasksListToDraggedItem(list)}
       onDrop={dropOnList}
--- src/App/widgets/TasksList/components/TasksListHeader/TasksListHeader.tsx
+++ src/App/widgets/TasksList/components/TasksListHeader/TasksListHeader.tsx
@@ -2,8 +2,6 @@ import { PinButton } from "App/components/PinButton/PinButton";
 import { RemoveButton } from "App/components/RemoveButton/RemoveButton";
 import { TasksListViewModel } from "App/entities/TasksList/TasksListViewModel";
 import { useTasksListFeatures } from "../../hooks/useTasksListFeatures";
-import { TasksListHeaderContainer } from "./components/TasksListHeaderContainer";
-import { TasksListTitle } from "./components/TasksListTitle";
 import style from "./TasksListHeader.module.css";
 
 interface TasksListHeaderProps {
@@ -13,10 +11,16 @@ export const TasksListHeader = ({ list }: TasksListHeaderProps) => {
   const { remove, togglePin } = useTasksListFeatures(list);
 
   return (
-    <TasksListHeaderContainer>
-      <PinButton isPinned={list.isPinned} onClick={togglePin} />
-      <TasksListTitle className={style.Title}>{list.title}</TasksListTitle>
+    <div className={style.container}>
+      <PinButton
+        className={style.PinButton}
+        isPinned={list.isPinned}
+        onClick={togglePin}
+      />
+
+      <h3 className={style.title}>{list.title}</h3>
+
       <RemoveButton className={style.RemoveButton} onClick={remove} />
-    </TasksListHeaderContainer>
+    </div>
   );
 };

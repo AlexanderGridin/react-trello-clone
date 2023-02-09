5e7dac7a55e3de7d6bdaad960c44c51399e424b4
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-02-09 11:54:27 +0200

    move TasksList features to related components

--- src/App/widgets/TasksList/TasksList.tsx
+++ src/App/widgets/TasksList/TasksList.tsx
@@ -1,10 +1,14 @@
 import { DndCard } from "App/components/DndCard/DndCard";
+import { AppDraggedItem } from "App/entities/AppDraggedItem/AppDraggedItem";
+import { useAppDraggedItemDispatchers } from "App/entities/AppDraggedItem/state/hooks/useAppDraggedItemDispatchers";
+import { useTaskDispatchers } from "App/entities/Task/state/hooks/useTaskDispatchers";
 import { mapTasksListToDraggedItem } from "App/entities/TasksList/mappers/mapTasksListToDraggedItem";
+import { useTasksListDispatchers } from "App/entities/TasksList/state/hooks/useTasksListDispatchers";
 import { TasksListViewModel } from "App/entities/TasksList/TasksListViewModel";
+import { DraggedItemType } from "App/enums/DraggedItemType";
 import { Card } from "shared/components/Card/Card";
 import { ListOfTasks } from "../ListOfTasks/ListOfTasks";
 import { TasksListHeader } from "./components/TasksListHeader/TasksListHeader";
-import { useTasksListFeatures } from "./hooks/useTasksListFeatures";
 
 export interface TasksListProps {
   list: TasksListViewModel;
@@ -13,7 +17,35 @@ export interface TasksListProps {
 
 export const TasksList = ({ list, isDragPreview = false }: TasksListProps) => {
   const BACKGROUD_COLOR = "#D8DEE9";
-  const { dropOnList } = useTasksListFeatures(list);
+
+  const { dispatchMoveTasksList, dispatchPushTaskInTasksList } =
+    useTasksListDispatchers();
+  const { dispatchRemoveTask } = useTaskDispatchers();
+  const { dispatchSetAppDraggedItem } = useAppDraggedItemDispatchers();
+
+  const dropOnList = (draggedItem: AppDraggedItem) => {
+    if (
+      draggedItem.type === DraggedItemType.TasksList &&
+      draggedItem.data.isPinned === list.isPinned
+    ) {
+      dispatchMoveTasksList(draggedItem.data, list);
+      return;
+    }
+
+    if (draggedItem.type !== DraggedItemType.Task || list.tasks.length) {
+      return;
+    }
+
+    dispatchRemoveTask(draggedItem.data);
+    dispatchPushTaskInTasksList(list, draggedItem.data);
+    dispatchSetAppDraggedItem({
+      ...draggedItem,
+      data: {
+        ...draggedItem.data,
+        listId: list.id,
+      },
+    });
+  };
 
   const header = <TasksListHeader list={list} />;
   const content = (

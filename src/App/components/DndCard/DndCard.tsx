af77d9f27c0fadee456f44c3218ced646982b87f
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-02-09 13:12:40 +0200

    DndCard updated

--- src/App/components/DndCard/DndCard.tsx
+++ src/App/components/DndCard/DndCard.tsx
@@ -1,13 +1,29 @@
 import { AppDraggedItem } from "App/entities/AppDraggedItem/AppDraggedItem";
+import { useItemDrag } from "App/hooks/useItemDrag";
+import { useItemDrop } from "App/hooks/useItemDrop";
+import { useRef } from "react";
 import { Card, CardProps } from "shared/components/Card/Card";
-import { useDndCardDragAndDrop } from "./hooks/useDndCardDragAndDrop";
 
 export interface DndCardProps extends CardProps {
   draggedItem: AppDraggedItem;
   onDrop: (draggedItem: AppDraggedItem) => void;
 }
 
-export const DndCard = (props: DndCardProps) => {
-  const { dragAndDropRef, draggingClassName } = useDndCardDragAndDrop(props);
-  return <Card ref={dragAndDropRef} className={draggingClassName} {...props} />;
+export const DndCard = ({
+  draggedItem,
+  onDrop,
+  ...restProps
+}: DndCardProps) => {
+  const { drag, isDragging } = useItemDrag({ ...draggedItem });
+  const className = isDragging ? "dragging" : "draggable";
+
+  const { drop } = useItemDrop({
+    ...draggedItem,
+    onDrop,
+  });
+
+  const ref = useRef<HTMLDivElement>(null);
+  drag(drop(ref));
+
+  return <Card ref={ref} className={className} {...restProps} />;
 };

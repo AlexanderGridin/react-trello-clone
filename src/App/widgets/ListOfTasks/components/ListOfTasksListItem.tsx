1866e414b54181b5c7ec16b9fe727d0c57077590
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-02-09 11:59:02 +0200

    move TaskFeatures to the related components

--- src/App/widgets/ListOfTasks/components/ListOfTasksListItem.tsx
+++ src/App/widgets/ListOfTasks/components/ListOfTasksListItem.tsx
@@ -1,7 +1,10 @@
 import { DndCard } from "App/components/DndCard/DndCard";
+import { AppDraggedItem } from "App/entities/AppDraggedItem/AppDraggedItem";
+import { useAppDraggedItemDispatchers } from "App/entities/AppDraggedItem/state/hooks/useAppDraggedItemDispatchers";
 import { mapTaskToDraggedItem } from "App/entities/Task/mappers/mapTaskToDraggedItem";
+import { useTaskDispatchers } from "App/entities/Task/state/hooks/useTaskDispatchers";
 import { TaskViewModel } from "App/entities/Task/TaskViewModel";
-import { useTaskFeatures } from "App/widgets/Task/hooks/useTaskFeatures";
+import { DraggedItemType } from "App/enums/DraggedItemType";
 import { Task } from "App/widgets/Task/Task";
 import { Card } from "shared/components/Card/Card";
 
@@ -15,7 +18,31 @@ export const ListOfTasksListItem = ({
   isDragPreview = false,
 }: ListOfTasksListItemProps) => {
   const BACKGROUD_COLOR = "#ECEFF4";
-  const { dropOnTask } = useTaskFeatures(task);
+
+  const { dispatchMoveTask } = useTaskDispatchers();
+  const { dispatchSetAppDraggedItem } = useAppDraggedItemDispatchers();
+
+  const dropOnTask = (draggedItem: AppDraggedItem) => {
+    if (draggedItem.type !== DraggedItemType.Task) {
+      return;
+    }
+
+    const draggedTask: TaskViewModel = draggedItem.data;
+
+    dispatchMoveTask(draggedTask, task);
+
+    if (draggedTask.listId === task.listId) {
+      return;
+    }
+
+    dispatchSetAppDraggedItem({
+      ...draggedItem,
+      data: {
+        ...draggedTask,
+        listId: task.listId,
+      },
+    });
+  };
 
   if (!task) {
     return null;
--- src/App/widgets/Task/Task.tsx
+++ src/App/widgets/Task/Task.tsx
@@ -1,14 +1,15 @@
 import { TaskViewModel } from "App/entities/Task/TaskViewModel";
 import { RemoveButton } from "App/components/RemoveButton/RemoveButton";
-import { useTaskFeatures } from "./hooks/useTaskFeatures";
 import style from "./Task.module.css";
+import { useTaskDispatchers } from "App/entities/Task/state/hooks/useTaskDispatchers";
 
 interface TaskProps {
   task: TaskViewModel;
 }
 
 export const Task = ({ task }: TaskProps) => {
-  const { removeTask } = useTaskFeatures(task);
+  const { dispatchRemoveTask } = useTaskDispatchers();
+  const removeTask = () => dispatchRemoveTask(task);
 
   return (
     <div className={style.container}>

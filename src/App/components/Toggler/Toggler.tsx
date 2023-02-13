aa46f5a2ed73c1f4ff29b85e4a38701ee99cec75
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-02-13 17:12:21 +0200

    Toggler added

---
+++ src/App/components/Toggler/Toggler.tsx
@@ -0,0 +1,38 @@
+import { useReducer } from "react";
+import { ButtonType } from "shared/components/Button/enums/ButtonType";
+import style from "./Toggler.module.css";
+
+interface TogglerProps {
+  initialValue?: boolean;
+  label?: string;
+  onToggle: (isActive: boolean) => void;
+}
+
+export const Toggler = ({
+  initialValue = false,
+  label = "",
+  onToggle,
+}: TogglerProps) => {
+  const [isActive, dispatch] = useReducer(
+    (_: boolean, payload: boolean) => !payload,
+    initialValue
+  );
+  const cn = `${style.button} ${isActive ? style.active : ""}`;
+
+  return (
+    <div className={style.container}>
+      <button
+        type={ButtonType.Button}
+        className={cn}
+        onClick={() => {
+          dispatch(isActive);
+          onToggle(!isActive);
+        }}
+      >
+        Toogler
+      </button>
+
+      {label && <span className={style.label}>{label}</span>}
+    </div>
+  );
+};

b277326cf5c733eb3e2795749c7279f404e9aafb
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-02-20 01:20:14 +0200

    Checkbox added

---
+++ src/shared/components/Form/Checkbox.tsx
@@ -0,0 +1,37 @@
+import MuiCheckbox from "@mui/material/Checkbox";
+import { styled } from "@mui/material/styles";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import { ChangeEvent } from "react";
+
+const StyledCheckbox = styled(MuiCheckbox)({
+  "& .MuiSvgIcon-root": {
+    fill: "#a5a9b1",
+  },
+
+  "&.Mui-checked .MuiSvgIcon-root": {
+    fill: "#5E81AC",
+  },
+});
+
+export interface CheckboxProps {
+  label?: string;
+  value: boolean;
+  onChange: (value: boolean) => void;
+}
+
+export const Checkbox = ({ label = "", value, onChange }: CheckboxProps) => {
+  const handleChange = (
+    _e: ChangeEvent<HTMLInputElement>,
+    checked: boolean
+  ) => {
+    onChange(checked);
+  };
+  return (
+    <div style={{ marginTop: "-9px", marginBottom: "-9px" }}>
+      <FormControlLabel
+        control={<StyledCheckbox value={value} onChange={handleChange} />}
+        label={label}
+      />
+    </div>
+  );
+};

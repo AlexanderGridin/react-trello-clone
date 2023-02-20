9bb765db52e228576da7a9e85fa45e4535eb8485
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-02-20 13:23:12 +0200

    FormFooter added

---
+++ src/shared/components/Form/FormFooter.tsx
@@ -0,0 +1,29 @@
+import { Button } from "shared/components/Button/Button";
+import { ButtonStyle } from "shared/components/Button/enums/ButtonStyle";
+import { FlexContainer } from "shared/components/FlexContainer";
+
+interface FormFooterProps {
+  submitText?: string;
+  cancelText?: string;
+  onSubmit: () => void;
+  onCancel: () => void;
+}
+
+export const FormFooter = ({
+  submitText = "Submit",
+  cancelText = "Cancel",
+  onSubmit,
+  onCancel,
+}: FormFooterProps) => {
+  return (
+    <FlexContainer>
+      <Button buttonStyle={ButtonStyle.Success} onClick={onSubmit}>
+        {submitText}
+      </Button>
+
+      <Button buttonStyle={ButtonStyle.Danger} onClick={onCancel}>
+        {cancelText}
+      </Button>
+    </FlexContainer>
+  );
+};

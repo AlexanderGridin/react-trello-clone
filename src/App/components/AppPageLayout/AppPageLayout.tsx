b845f3d0bf868ff7a2b83268fea83d94420a0f32
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-02-25 00:55:47 +0200

    AddPageLayout tests written

--- src/App/components/AppPageLayout/AppPageLayout.tsx
+++ src/App/components/AppPageLayout/AppPageLayout.tsx
@@ -2,6 +2,9 @@ import { ReactNode } from "react";
 import { Spinner } from "shared/components/Spinner/Spinner";
 import { Children } from "shared/models/Children";
 import style from "./AppPageLayout.module.css";
+import { AppPageLayoutTestId } from "./static-data/AppPageLayoutTestId";
+
+const { Layout: LayoutId, Header: HeaderId, Content: ContentId, Spinner: SpinnerId } = AppPageLayoutTestId;
 
 interface AppPageLayoutProps extends Children {
   slotHeader?: ReactNode;
@@ -10,14 +13,17 @@ interface AppPageLayoutProps extends Children {
 
 export const AppPageLayout = ({ slotHeader, isLoading = false, children }: AppPageLayoutProps) => {
   return (
-    <>
-      <section className={style.container}>
-        {slotHeader && <header className={style.header}>{slotHeader}</header>}
-        <div className={style.content}>
-          {children}
-          {isLoading && <Spinner />}
-        </div>
-      </section>
-    </>
+    <section data-testid={LayoutId} className={style.container}>
+      {slotHeader && (
+        <header data-testid={HeaderId} className={style.header}>
+          {slotHeader}
+        </header>
+      )}
+
+      <div data-testid={ContentId} className={style.content}>
+        {children}
+        {isLoading && <Spinner data-testid={SpinnerId} />}
+      </div>
+    </section>
   );
 };

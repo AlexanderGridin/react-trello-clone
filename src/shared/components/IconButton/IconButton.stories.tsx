2a0aa5d584c205791eacfa05ae9920c96d8ba4ae
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-02-26 23:43:41 +0200

    IconButton added

---
+++ src/shared/components/IconButton/IconButton.stories.tsx
@@ -0,0 +1,59 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { parseEnumForStory } from "shared/utils/storybook/parseEnumForStory";
+import { MaterialIcon } from "../Icon/enums/MaterialIcon";
+import { IconButton } from "./IconButton";
+
+const { options: materialIconOptions, labels: materialIconLabels } = parseEnumForStory(MaterialIcon);
+
+export default {
+  title: "Shared/IconButton",
+  component: IconButton,
+  args: {
+    icon: MaterialIcon.Home,
+    type: "button",
+    isActive: false,
+    activeColor: "#5E81AC",
+  },
+  argTypes: {
+    type: {
+      description: "HTML5 button type",
+    },
+    isActive: {
+      description: "Defines is active color will be visible",
+    },
+    activeColor: {
+      description: "Active color, that will be used on hover, and when button will be marked as active",
+    },
+    className: {
+      description: "String with additional CSS classes",
+    },
+    "data-testid": {
+      description: "Id that will be used in the testing",
+    },
+    onClick: {
+      description: "Callback that will be called when button will be clicked",
+    },
+    icon: {
+      description: "Icon that will be displayed",
+      options: materialIconOptions,
+      mapping: MaterialIcon,
+      control: {
+        labels: materialIconLabels,
+      },
+      table: {
+        type: {
+          summary: "MaterialIcon",
+          detail: Object.keys(MaterialIcon).join("\n"),
+        },
+        defaultValue: {
+          summary: "Home",
+        },
+      },
+    },
+  },
+} as ComponentMeta<typeof IconButton>;
+
+const Template: ComponentStory<typeof IconButton> = (args) => <IconButton {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};

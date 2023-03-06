299f319ea766f8c3d100b9883c652abf616bdafb
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-03-06 17:41:35 +0200

    Boards mock updated

--- src/App/api/mock/Boards/initBoardsMock.ts
+++ src/App/api/mock/Boards/initBoardsMock.ts
@@ -2,8 +2,9 @@ import MockAdapter from "axios-mock-adapter";
 import { routes } from "App/api/Boards/routes";
 import { logMockResponse } from "../utils/logMockResponse";
 import { connectToMockDb } from "../mockDb";
+import { generateId } from "shared/utils/generateId";
 
-const { boards: boardsDb } = connectToMockDb();
+const { boards: boardsDb, user: userDb } = connectToMockDb();
 
 export const initBoardsMock = (adapter: MockAdapter) => {
   adapter.onGet(routes.getAllBoards).reply(() => {
@@ -28,5 +29,68 @@ export const initBoardsMock = (adapter: MockAdapter) => {
     });
 
     return [200, boards];
+  });
+
+  adapter.onPost(routes.addBoard).reply((config) => {
+    const board = {
+      _id: generateId(),
+      ...JSON.parse(config.data),
+      user: userDb.get(),
+    };
+
+    boardsDb.push(board);
+
+    logMockResponse({
+      method: "POST",
+      url: routes.addBoard,
+      response: board,
+    });
+
+    return [200, board];
+  });
+
+  const urlRegExp = /\/boards\/[a-z0-9]/i;
+
+  adapter.onGet(urlRegExp).reply((config) => {
+    const id = config.url?.split("/")?.[2] ?? generateId();
+    const board = boardsDb.getById(id);
+
+    logMockResponse({
+      method: "GET",
+      url: routes.getBoard.replace("{$boardId}", id),
+      response: board,
+    });
+
+    return [200, board];
+  });
+
+  adapter.onPut(urlRegExp).reply((config) => {
+    const id = config.url?.split("/")?.[2] ?? generateId();
+    const board = { ...boardsDb.getById(id), ...JSON.parse(config.data), user: userDb.get() };
+
+    boardsDb.update(board);
+
+    logMockResponse({
+      method: "PUT",
+      url: routes.updateBoard.replace("{$boardId}", id),
+      response: board,
+    });
+
+    return [200, board];
+  });
+
+  adapter.onDelete(urlRegExp).reply((config) => {
+    const id = config.url?.split("/")?.[2] ?? generateId();
+    const board = boardsDb.getById(id);
+
+    boardsDb.delete(board);
+
+    logMockResponse({
+      method: "DELETE",
+      url: routes.removeBoard.replace("{$boardId}", id),
+      response: board,
+    });
+
+    return [200, board];
   });
 };

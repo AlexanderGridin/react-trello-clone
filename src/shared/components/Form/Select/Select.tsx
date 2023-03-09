9169572a0d1c418ec39d20fafd78f460c9aca2be
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-03-10 00:06:14 +0200

    Select updated

--- src/shared/components/Form/Select/Select.tsx
+++ src/shared/components/Form/Select/Select.tsx
@@ -1,28 +1,30 @@
 import MuiInputLabel from "@mui/material/InputLabel";
 import MuiFormControl from "@mui/material/FormControl";
 import { SelectChangeEvent } from "@mui/material/Select";
-import { ReactNode } from "react";
+import { ChangeEvent, ReactNode } from "react";
 import { MuiSelect } from "./components/MuiSelect";
 import { MenuItem } from "./components/MenuItem";
 import { SelectDataItem } from "./models/SelectDataItem";
 
 interface SelectProps {
-  value?: string;
+  id: string;
+  name: string;
+  value: string;
   label?: string;
-  data?: SelectDataItem[];
-  onChange?: (value: string) => void;
+  data: SelectDataItem[];
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const Select = ({ value, label, data = [], onChange }: SelectProps) => {
+export const Select = ({ id, name, value, label, data, onChange }: SelectProps) => {
   const handleChange = (event: SelectChangeEvent<unknown>, _child: ReactNode) => {
-    onChange?.(event.target.value as string);
+    onChange(event as ChangeEvent<HTMLInputElement>);
   };
 
   return (
     <MuiFormControl fullWidth>
-      {label && <MuiInputLabel id="demo-simple-select-standard-label">{label}</MuiInputLabel>}
+      {label && <MuiInputLabel id={`${id}__label`}>{label}</MuiInputLabel>}
 
-      <MuiSelect value={value} label={label} onChange={handleChange}>
+      <MuiSelect id={id} name={name} value={value} label={label} onChange={handleChange}>
         {data.map((item: SelectDataItem, index: number) => (
           <MenuItem key={index} value={item.value}>
             {item.name}

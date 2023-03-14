99307e74478d507119756b0234e6852720f60307
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-03-14 20:11:01 +0200

    Chip updated

--- src/shared/components/Chip/Chip.tsx
+++ src/shared/components/Chip/Chip.tsx
@@ -1,7 +1,7 @@
 import MuiChip from "@mui/material/Chip";
 import { IChildren, IClassName } from "shared/models";
 
-interface IChipProps extends IChildren<string>, IClassName {}
+interface IChipProps extends IChildren, IClassName {}
 
 export const Chip = ({ className, children }: IChipProps) => {
   return <MuiChip label={children} className={className} sx={{ fontSize: "14px" }} />;

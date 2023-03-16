2462c95d97abd25473ba4458b84763fc6ce7cf2b
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-03-16 22:22:33 +0200

    Table component updated

--- src/shared/components/Table/Table.tsx
+++ src/shared/components/Table/Table.tsx
@@ -1,26 +1,16 @@
-import { ReactNode } from "react";
 import MuiTable from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
+
+import { Spinner } from "shared/components/Spinner/Spinner";
 import { IChildren } from "shared/models";
-import { Spinner } from "../Spinner/Spinner";
 
-export type TAlign = "left" | "right" | "center" | "justify";
-
-export interface ITableColumn<Row> {
-  title: string;
-  field: keyof Row | "actions";
-  align?: TAlign;
-  cellAlign?: TAlign;
-}
-
-const CustomPapper = (props: any) => <Paper elevation={6} {...props} />;
-
-type TTableChildren<Row> = (cellValue: Row[keyof Row], fieldName?: keyof Row | "actions", row?: Row) => ReactNode;
+import { TablePapper } from "./components";
+import { ITableColumn } from "./models";
+import { TTableChildren } from "./types";
 
 interface ITableProps<Row> extends IChildren<TTableChildren<Row>> {
   columns: ITableColumn<Row>[];
@@ -31,7 +21,7 @@ interface ITableProps<Row> extends IChildren<TTableChildren<Row>> {
 export function Table<Row>({ columns, rows, isLoading = false, children }: ITableProps<Row>) {
   return (
     <div style={{ position: "relative", borderRadius: "4px", overflow: "hidden" }}>
-      <TableContainer component={CustomPapper} sx={{ maxWidth: "850px", minHeight: "350px" }}>
+      <TableContainer component={TablePapper} sx={{ maxWidth: "850px", minHeight: "350px" }}>
         <MuiTable>
           <TableHead>
             <TableRow>

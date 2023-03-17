5ec0e9db570521934f3e2bfdd3e7409bb0ef32cc
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-03-17 17:32:51 +0200

    AddBoardButton updated

--- src/App/widgets/boards/AddBoard/components/AddBoardButton.tsx
+++ src/App/widgets/boards/AddBoard/components/AddBoardButton.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { IChildren, IClick } from "shared/models";
 import { StyledAddButton } from "App/components/StyledAddButton";
 
-const Container = styled(StyledAddButton)`
+const StyledAddBoardButton = styled(StyledAddButton)`
   min-height: 150px;
   color: #fff;
   display: flex;
@@ -16,8 +16,8 @@ interface IAddBoardButtonProps extends IChildren, IClick {}
 
 export const AddBoardButton = ({ children, onClick }: IAddBoardButtonProps) => {
   return (
-    <Container type="button" onClick={onClick}>
+    <StyledAddBoardButton type="button" onClick={onClick}>
       {children}
-    </Container>
+    </StyledAddBoardButton>
   );
 };

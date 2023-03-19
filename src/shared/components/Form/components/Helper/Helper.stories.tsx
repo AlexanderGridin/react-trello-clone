26c00fd2cdaf5eb318e1cd362e7c10c3d7e030a0
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-03-19 21:53:00 +0200

    Helper.stories updated

--- src/shared/components/Form/components/Helper/Helper.stories.tsx
+++ src/shared/components/Form/components/Helper/Helper.stories.tsx
@@ -8,15 +8,29 @@ export default {
     children: "Test helper text",
     type: "regular",
   },
+  argTypes: {
+    children: {
+      description: "Accepts ReactNode that will be displayed as Helper content",
+      type: { name: "string", required: true },
+    },
+    type: {
+      description: "Defines visual style of the Helper",
+    },
+  },
 } as ComponentMeta<typeof Helper>;
 
 const Template: ComponentStory<typeof Helper> = (args) => <Helper {...args} />;
 
-export const Default = Template.bind({});
-Default.args = {};
+export const AllProps = Template.bind({});
+AllProps.args = {};
 
-export const WithComplexText = Template.bind({});
-WithComplexText.args = {
+export const WithComplexChildren = Template.bind({});
+
+WithComplexChildren.argTypes = {
+  children: { control: false },
+};
+
+WithComplexChildren.args = {
   children: (
     <span>
       <b>Bold</b> text and <u>underlined</u> text.
